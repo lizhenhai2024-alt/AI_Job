f7ced630118b06b536b5ecd97af5711529db33a9
Zhenhai Li/Bruce <lizhenhai2024@gmail.com>
2026-09-08 16:17:17 +0800

    feat: broaden default target role families

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,6 +1,9 @@
 export const defaultProfile = {
   graduationYear: '2027',
-  targetRoles: ['海外运营', 'GTM', '产品营销', '用户运营'],
+  targetRoles: [
+    '海外运营', 'GTM', '产品营销', '用户运营',
+    '电商运营', '内容运营', '产品运营', '市场', '业务运营'
+  ],
   targetCities: ['深圳', '上海', '广州'],
   skills: ['英语', 'Excel', '数据分析', '内容运营'],
   languages: ['英语'],
